2eb696bfbc92c15ed23476fa7a19de2fe6e37679
Jhonny Claure <claurejhonny188@gmail.com>
2024-02-29 13:41:58 +0100

    update

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ let menuIcon = document.querySelector(".header__icon--menu");
 let menuResposive = document.querySelector(".header__menu")
 
 menuIcon.addEventListener("click", () => {
+  menuResposive.classList.toogle("hide-1");
   menuResposive.classList.toggle("hide");
+
 })
